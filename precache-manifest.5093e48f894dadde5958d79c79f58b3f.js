8c4edbe2b72b19a844f070ad33e5ff767189776f
Ivan Komar <komar.ioan@gmail.com>
2019-11-19 20:41:04 +0200

    Updates

---
+++ precache-manifest.5093e48f894dadde5958d79c79f58b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbdb1a384d9a9dbd8efba4460e7b867",
+    "url": "/DairyApp/index.html"
+  },
+  {
+    "revision": "b38797fd3356755ed58d",
+    "url": "/DairyApp/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a7dfd5b8fb39a2953d1a",
+    "url": "/DairyApp/static/css/main.12181124.chunk.css"
+  },
+  {
+    "revision": "b38797fd3356755ed58d",
+    "url": "/DairyApp/static/js/2.57a734a3.chunk.js"
+  },
+  {
+    "revision": "a7dfd5b8fb39a2953d1a",
+    "url": "/DairyApp/static/js/main.9c3edaa0.chunk.js"
+  },
+  {
+    "revision": "86772a496ee2910d9790",
+    "url": "/DairyApp/static/js/runtime-main.bf6e6cee.js"
+  }
+]);
